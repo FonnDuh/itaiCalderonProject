47ba3ac5f3c777ed0ee1a0465558f7536b4bc524
FonnDuh <itaic1901@gmail.com>
2024-10-01 18:56:31 +0300

    Updated userManager Project

--- projects/JS_DOM_Mission_userManager/script/script.js
+++ projects/JS_DOM_Mission_userManager/script/script.js
@@ -1,5 +1,5 @@
-let users,
-  firstName = document.getElementById("firstName"),
+let users;
+const firstName = document.getElementById("firstName"),
   lastName = document.getElementById("lastName"),
   email = document.getElementById("email"),
   password = document.getElementById("password"),
@@ -14,83 +14,52 @@ if (typeof Storage !== "undefined") {
 }
 
 if (users != null) {
-  for (const element of users) {
-    addToTable(
-      element.fName,
-      element.lName,
-      element.email,
-      element.pass,
-      element.connected
-    );
-  }
+  users.map((user) => {
+    addToTable(user.fName, user.lName, user.email, user.pass, user.connected);
+  });
 }
 
-document
-  .getElementById("signinForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent form submission
+document.getElementById("signinForm").addEventListener("submit", (event) => {
+  event.preventDefault(); // Prevent form submission
 
-    if (typeof Storage !== "undefined") {
-      const user = users.find(
-        (user) =>
-          user.email == signinEmail.value && user.pass == signinPassword.value
-      );
-
-      if (user) {
-        if (user.connected == "מחובר") {
-          alert("משתמש כבר מחובר");
-          return;
-        }
-        user.connected = "מחובר";
-        localStorage.setItem("users", JSON.stringify(users));
-        document.getElementById("tbody").innerHTML = "";
-        for (const element of users) {
-          addToTable(
-            element.fName,
-            element.lName,
-            element.email,
-            element.pass,
-            element.connected
-          );
-        }
-        return;
-      }
-      alert("משתמש לא קיים");
-    } else {
-      alert("Sorry, your browser does not support Web Storage...");
-    }
-  });
-
-function login() {
   if (typeof Storage !== "undefined") {
-    if (signinEmail.value == "" && signinPassword.value == "") {
-      return;
-    } else if (users == null) {
-      alert("משתמש לא קיים");
-      return;
-    }
+    const user = users.find(
+      (user) =>
+        user.email == signinEmail.value && user.pass == signinPassword.value
+    );
 
-    for (const element of users) {
-      if (
-        signinEmail.value.toLowerCase() == element.email.toLowerCase() &&
-        signinPassword.value.toLowerCase() == element.pass.toLowerCase()
-      ) {
-        if (element.connected == "מחובר") {
-          alert("משתמש כבר מחובר");
-          return;
-        }
-        element.connected = "מחובר";
-        localStorage.setItem("users", JSON.stringify(users));
+    if (user) {
+      if (user.connected == "מחובר") {
+        signinEmail.value = "";
+        signinPassword.value = "";
+        alert("משתמש כבר מחובר");
         return;
       }
+      user.connected = "מחובר";
+      localStorage.setItem("users", JSON.stringify(users));
+      document.getElementById("tbody").innerHTML = "";
+      users.map((user) => {
+        addToTable(
+          user.fName,
+          user.lName,
+          user.email,
+          user.pass,
+          user.connected
+        );
+      });
+    } else {
+      signinEmail.value = "";
+      signinPassword.value = "";
+      alert("משתמש לא קיים");
     }
-    alert("משתמש לא קיים");
   } else {
     alert("Sorry, your browser does not support Web Storage...");
   }
-}
+});
 
-function addUser() {
+document.getElementById("signupForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+
   if (typeof Storage !== "undefined") {
     if (
       firstName.value == "" &&
@@ -107,22 +76,25 @@ function addUser() {
           lName: lastName.value,
           email: email.value,
           pass: password.value,
-          connected: "מחובר",
+          connected: "מנותק",
         },
       ];
     } else {
-      for (const element of users) {
-        if (element.email.toLowerCase() == email.value.toLowerCase()) {
-          alert("השם משתמש הזה כבר תפוס. בחר/י שם משתמש אחר.");
-          return;
-        }
+      if (
+        users.some(
+          (user) => user.email.toLowerCase() === email.value.toLowerCase()
+        )
+      ) {
+        alert("השם משתמש הזה כבר תפוס. בחר/י שם משתמש אחר.");
+        return;
       }
+
       users.push({
         fName: firstName.value,
         lName: lastName.value,
         email: email.value,
         pass: password.value,
-        connected: "מחובר",
+        connected: "מנותק",
       });
     }
     localStorage.setItem("users", JSON.stringify(users));
@@ -131,78 +103,16 @@ function addUser() {
       lastName.value,
       email.value,
       password.value,
-      "מחובר"
+      "מנותק"
     );
+    firstName.value = "";
+    lastName.value = "";
+    email.value = "";
+    password.value = "";
   } else {
     alert("Sorry, your browser does not support Web Storage...");
   }
-}
-
-function disconnect(tag) {
-  let index = tag.parentNode.parentNode.rowIndex;
-  if (confirm("את/ה בטוח שברצונך לנתק את המשתמש?")) {
-    let row = document.getElementById("table").rows[index];
-    for (const element of users) {
-      if (
-        row.cells[0].innerHTML.toLowerCase() == element.fName.toLowerCase() &&
-        row.cells[1].innerHTML.toLowerCase() == element.lName.toLowerCase() &&
-        row.cells[2].innerHTML.toLowerCase() == element.email.toLowerCase() &&
-        row.cells[3].innerHTML.toLowerCase() == element.pass.toLowerCase()
-      ) {
-        element.connected = "מנותק";
-        localStorage.setItem("users", JSON.stringify(users));
-        row.cells[4].innerHTML = "מנותק";
-        tag.remove();
-        return;
-      }
-    }
-  }
-}
-
-function editUser(tag) {
-  let index = tag.parentNode.parentNode.rowIndex,
-    row = document.getElementById("table").rows[index],
-    userIndex;
-  for (let i in users) {
-    if (
-      row.cells[0].innerHTML.toLowerCase() == users[i].fName.toLowerCase() &&
-      row.cells[1].innerHTML.toLowerCase() == users[i].lName.toLowerCase() &&
-      row.cells[2].innerHTML.toLowerCase() == users[i].email.toLowerCase() &&
-      row.cells[3].innerHTML.toLowerCase() == users[i].pass.toLowerCase()
-    ) {
-      userIndex = i;
-    }
-  }
-  row.cells[0].innerHTML = `<input type="text" id="tempFname${userIndex}" class="form-control" value = "${row.cells[0].innerHTML}" />`;
-  row.cells[1].innerHTML = `<input type="text" id="tempLname${userIndex}" class="form-control" value = "${row.cells[1].innerHTML}" />`;
-  row.cells[2].innerHTML = `<input type="email" id="tempMail${userIndex}" class="form-control" value = "${row.cells[2].innerHTML}" />`;
-  row.cells[3].innerHTML = `<input type="password" id="tempPass${userIndex}" class="form-control" value = "${row.cells[3].innerHTML}" />`;
-  tag.innerHTML = `<img src="./images/check_16px.svg" alt="Check Icon"> אישור`;
-
-  tag.onclick = () => {
-    row.cells[0].innerHTML = document.getElementById(
-      `tempFname${userIndex}`
-    ).value;
-    row.cells[1].innerHTML = document.getElementById(
-      `tempLname${userIndex}`
-    ).value;
-    row.cells[2].innerHTML = document.getElementById(
-      `tempMail${userIndex}`
-    ).value;
-    row.cells[3].innerHTML = document.getElementById(
-      `tempPass${userIndex}`
-    ).value;
-
-    users[userIndex].fName = row.cells[0].innerHTML;
-    users[userIndex].lName = row.cells[1].innerHTML;
-    users[userIndex].email = row.cells[2].innerHTML;
-    users[userIndex].pass = row.cells[3].innerHTML;
-    localStorage.setItem("users", JSON.stringify(users));
-
-    tag.innerHTML = `<img src="./images/edit_16px.svg" alt="Edit Icon"> עריכה`;
-    tag.setAttribute("onclick", "editUser(this)");
-  };
-}
+});
 
 function addToTable(
   userFirstName,
@@ -211,7 +121,7 @@ function addToTable(
   userPassword,
   isConnected
 ) {
-  let row = document.getElementById("tbody").insertRow(-1),
+  const row = document.getElementById("tbody").insertRow(-1),
     fname = row.insertCell(0),
     lname = row.insertCell(1),
     mail = row.insertCell(2),
@@ -224,30 +134,165 @@ function addToTable(
   mail.innerHTML = userMail;
   pass.innerHTML = userPassword;
   status.innerHTML = isConnected;
-  if (isConnected == "מחובר") {
-    btn.innerHTML = `<button class="tableBtn btn btn-outline-warning" onclick="disconnect(this)"><img src="./images/logout_16px.svg" alt="Exit Icon"> התנתקות</button>`;
-  }
+  btn.innerHTML =
+    isConnected == "מחובר"
+      ? `<button class="tableBtn btn btn-outline-warning" onclick="disconnect(this)"><img src="./images/logout_16px.svg" alt="Exit Icon"> התנתקות</button>`
+      : "";
   btn.innerHTML +=
     `<button class="tableBtn btn btn-outline-danger" onclick="removeRow(this)"><img src="./images/delete_16px.svg" alt="Delete Icon"> מחיקה</button>` +
     `<button class="tableBtn btn btn-outline-primary" onclick="editUser(this)"><img src="./images/edit_16px.svg" alt="Edit Icon"> עריכה</button>`;
 }
 
-function removeRow(tag) {
-  let index = tag.parentNode.parentNode.rowIndex;
-  if (confirm("את/ה בטוח שברצונך למחוק את המשתמש?")) {
-    let row = document.getElementById("table").rows[index];
-    for (let i in users) {
+function disconnect(tag) {
+  const row =
+      document.getElementById("table").rows[tag.parentNode.parentNode.rowIndex],
+    userIndex = users.findIndex(
+      (user) =>
+        user.fName.toLowerCase() === row.cells[0].innerHTML.toLowerCase() &&
+        user.lName.toLowerCase() === row.cells[1].innerHTML.toLowerCase() &&
+        user.email.toLowerCase() === row.cells[2].innerHTML.toLowerCase() &&
+        user.pass === row.cells[3].innerHTML
+    );
+
+  if (confirm("את/ה בטוח שברצונך לנתק את המשתמש?")) {
+    users[userIndex].connected = "מנותק";
+    localStorage.setItem("users", JSON.stringify(users));
+    row.cells[4].innerHTML = "מנותק";
+    tag.remove();
+  }
+}
+
+function editUser(tag) {
+  const row =
+      document.getElementById("table").rows[tag.parentNode.parentNode.rowIndex],
+    tablefName = row.cells[0],
+    tablelName = row.cells[1],
+    tableMail = row.cells[2],
+    tablePass = row.cells[3],
+    userIndex = users.findIndex(
+      (user) =>
+        user.fName.toLowerCase() === tablefName.innerHTML.toLowerCase() &&
+        user.lName.toLowerCase() === tablelName.innerHTML.toLowerCase() &&
+        user.email.toLowerCase() === tableMail.innerHTML.toLowerCase() &&
+        user.pass === tablePass.innerHTML
+    );
+
+  if (tag.innerHTML.includes("עריכה")) {
+    tablefName.innerHTML = `<input type="text" id="tempFname${userIndex}" class="form-control" value="${tablefName.innerHTML}" />`;
+    tablelName.innerHTML = `<input type="text" id="tempLname${userIndex}" class="form-control" value="${tablelName.innerHTML}" />`;
+    tableMail.innerHTML = `<input type="email" id="tempMail${userIndex}" class="form-control" value="${tableMail.innerHTML}" />`;
+    tablePass.innerHTML = `<input type="password" id="tempPass${userIndex}" class="form-control" value="${tablePass.innerHTML}" />`;
+    tag.innerHTML = `<img src="./images/check_16px.svg" alt="Check Icon"> אישור`;
+
+    tag.onclick = () => {
+      const fNameInput = document.getElementById(`tempFname${userIndex}`),
+        lNameInput = document.getElementById(`tempLname${userIndex}`),
+        emailInput = document.getElementById(`tempMail${userIndex}`),
+        passInput = document.getElementById(`tempPass${userIndex}`);
+
       if (
-        row.cells[0].innerHTML == users[i].fName &&
-        row.cells[1].innerHTML == users[i].lName &&
-        row.cells[2].innerHTML == users[i].email &&
-        row.cells[3].innerHTML == users[i].pass
+        fNameInput &&
+        lNameInput &&
+        emailInput &&
+        passInput &&
+        fNameInput.value !== "" &&
+        lNameInput.value !== "" &&
+        emailInput.value !== "" &&
+        passInput.value !== ""
       ) {
-        users.splice(i, 1);
+        users[userIndex].fName = fNameInput.value;
+        users[userIndex].lName = lNameInput.value;
+        users[userIndex].email = emailInput.value;
+        users[userIndex].pass = passInput.value;
         localStorage.setItem("users", JSON.stringify(users));
-        table.deleteRow(index);
-        return;
+
+        tablefName.innerHTML = users[userIndex].fName;
+        tablelName.innerHTML = users[userIndex].lName;
+        tableMail.innerHTML = users[userIndex].email;
+        tablePass.innerHTML = users[userIndex].pass;
+
+        tag.innerHTML = `<img src="./images/edit_16px.svg" alt="Edit Icon"> עריכה`;
+        tag.onclick = () => editUser(tag);
       }
-    }
+    };
+  }
+}
+
+// function editUser(tag) {
+//    const row =
+//    document.getElementById("table").rows[tag.parentNode.parentNode.rowIndex],
+//   (tablefName = row.cells[0]),
+//   (tablelName = row.cells[1]),
+//   (tableMail = row.cells[2]),
+//   (tablePass = row.cells[3]),
+//   (userIndex = users.findIndex(
+//     (user) =>
+//       user.fName.toLowerCase() === tablefName.innerHTML.toLowerCase() &&
+//       user.lName.toLowerCase() === tablelName.innerHTML.toLowerCase() &&
+//       user.email.toLowerCase() === tableMail.innerHTML.toLowerCase() &&
+//       user.pass === tablePass.innerHTML
+//   ));
+
+//   tablefName.innerHTML = `<input type="text" id="tempFname${userIndex}" class="form-control" value = "${tablefName.innerHTML}" />`;
+//   tablelName.innerHTML = `<input type="text" id="tempLname${userIndex}" class="form-control" value = "${tablelName.innerHTML}" />`;
+//   tableMail.innerHTML = `<input type="email" id="tempMail${userIndex}" class="form-control" value = "${tableMail.innerHTML}" />`;
+//   tablePass.innerHTML = `<input type="password" id="tempPass${userIndex}" class="form-control" value = "${tablePass.innerHTML}" />`;
+//   tag.innerHTML = `<img src="./images/check_16px.svg" alt="Check Icon"> אישור`;
+
+//   tag.onclick = () => {
+//     if (
+//       document.getElementById(`tempFname${userIndex}`) &&
+//       document.getElementById(`tempLname${userIndex}`) &&
+//       document.getElementById(`tempMail${userIndex}`) &&
+//       document.getElementById(`tempPass${userIndex}`)
+//     ) {
+//       if (
+//         tablefName.innerHTML != "" &&
+//         tablelName.innerHTML != "" &&
+//         tableMail.innerHTML != "" &&
+//         tablePass.innerHTML != ""
+//       ) {
+//         tablefName.innerHTML = document.getElementById(
+//           `tempFname${userIndex}`
+//         ).value;
+//         tablelName.innerHTML = document.getElementById(
+//           `tempLname${userIndex}`
+//         ).value;
+//         tableMail.innerHTML = document.getElementById(
+//           `tempMail${userIndex}`
+//         ).value;
+//         tablePass.innerHTML = document.getElementById(
+//           `tempPass${userIndex}`
+//         ).value;
+
+//         users[userIndex].fName = tablefName.innerHTML;
+//         users[userIndex].lName = tablelName.innerHTML;
+//         users[userIndex].email = tableMail.innerHTML;
+//         users[userIndex].pass = tablePass.innerHTML;
+//         localStorage.setItem("users", JSON.stringify(users));
+//         tag.innerHTML = `<img src="./images/edit_16px.svg" alt="Edit Icon"> עריכה`;
+//         tag.setAttribute("onclick", "editUser(this)");
+//       }
+//     } else {
+//       console.error("One or more input elements not found");
+//     }
+//   };
+// }
+
+function removeRow(tag) {
+  const index = tag.parentNode.parentNode.rowIndex;
+
+  if (confirm("את/ה בטוח שברצונך למחוק את המשתמש?")) {
+    const row = document.getElementById("table").rows[index],
+      userIndex = users.findIndex(
+        (user) =>
+          user.fName.toLowerCase() === row.cells[0].innerHTML.toLowerCase() &&
+          user.lName.toLowerCase() === row.cells[1].innerHTML.toLowerCase() &&
+          user.email.toLowerCase() === row.cells[2].innerHTML.toLowerCase() &&
+          user.pass === row.cells[3].innerHTML
+      );
+    users.splice(userIndex, 1);
+    localStorage.setItem("users", JSON.stringify(users));
+    table.deleteRow(index);
   }
 }
